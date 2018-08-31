996643650758059bd2af9832d26d1c861a623144
Daniel Jalkut <jalkut@red-sweater.com>
2018-08-31 10:29:45 -0400

    Update to Swift 4.2.

--- Blockpass/AppDelegate.swift
+++ Blockpass/AppDelegate.swift
@@ -21,26 +21,20 @@ class AppDelegate: NSObject, NSApplicationDelegate, RSStringMatchingKeyboardTapD
 	let keychainItemName = "Blockpass"
 	let keychainAccountName = "main password"
 
-	func saveSecretTextToKeychain(secretText : String) -> Void
+	func saveSecretTextToKeychain(_ secretText : String) -> Void
 	{
 		RSKeychain.setSecureString(secretText, forItemName:keychainItemName, accountName:keychainAccountName)
 	}
 
 	func getSecretTextFromKeychain() -> String?
 	{
-		return RSKeychain.secureStringForItemName(keychainItemName, accountName:keychainAccountName)
+		return RSKeychain.secureString(forItemName: keychainItemName, accountName:keychainAccountName)
 	}
 
 	@IBAction func dismissPasswordPrompt(sender: AnyObject)
 	{
-		// We don't have a way of preventing compilation based on SDK version, 
-		// so we have to rely upon Swift version correlating to the pertinent SDK 
-		// version. sender.tag is a method prior to 10.12 SDK, and a property thereafter.
-		#if swift(>=2.3)
-			NSApp.stopModalWithCode(sender.tag)
-		#else
-			NSApp.stopModalWithCode(sender.tag())
-		#endif
+		let response = NSApplication.ModalResponse(sender.tag)
+		NSApp.stopModal(withCode: response)
 	}
 
 	func passwordByPromptingUser() -> String?
@@ -50,21 +44,19 @@ class AppDelegate: NSObject, NSApplicationDelegate, RSStringMatchingKeyboardTapD
 		if (self.passwordPrompt == nil)
 		{
 			let panelNib : NSNib = NSNib(nibNamed:"PasswordPromptPanel", bundle:nil)!
-			panelNib.instantiateWithOwner(self, topLevelObjects:&passwordPromptNibObjects)
+		    panelNib.instantiate(withOwner: self, topLevelObjects: &passwordPromptNibObjects)
 		}
 
 		let thePrompt = self.passwordPrompt!
 		// Present the panel modally and at high layer since we're a faceless background app
 		thePrompt.center()
 
-		// NSStatusWindowLevel doesn't seem available in Swift? And the types for CG constants 
-		// are mismatched Int vs Int32 so we have to do this dance
-		thePrompt.level = Int(CGWindowLevelForKey(CGWindowLevelKey.StatusWindowLevelKey));
+		thePrompt.level = .statusBar
 
 		thePrompt.makeKeyAndOrderFront(nil)
 
-		let promptStatus = NSApp.runModalForWindow(thePrompt)
-		if (promptStatus == NSModalResponseOK)
+		let promptStatus = NSApp.runModal(for: thePrompt)
+		if (promptStatus == .OK)
 		{
 			userPassword = self.secureStringTextField!.stringValue
 		}
@@ -79,36 +71,29 @@ class AppDelegate: NSObject, NSApplicationDelegate, RSStringMatchingKeyboardTapD
 	{
 		// Make sure we have ability to intercept keys
 		let promptFlag = kAXTrustedCheckOptionPrompt.takeRetainedValue() as NSString
-		let myDict: CFDictionary = [promptFlag: true]
+		let myDict: CFDictionary = NSDictionary(dictionary: [promptFlag: true])
 		AXIsProcessTrustedWithOptions(myDict)
 
 		// Unfortunately the prompting mechanism doesn't seem to work from an LSUIElement app
 		if (!AXIsProcessTrustedWithOptions(myDict))
 		{
 			NSLog("Don't have accessibility access, so we're prompting...")
-			let warnAlert = NSAlert();
+			let warnAlert = NSAlert()
 			warnAlert.messageText = "Blockpass relies upon having permission to 'control your computer'. If the permission prompt did not appear automatically, go to System Preferences, Security & Privacy, Privacy, Accessibility, and add Blockpass to the list of allowed apps. Then relaunch Blockpass.";
 			warnAlert.layout()
 			let warnPanel = warnAlert.window as! NSPanel
-			warnPanel.level = Int(CGWindowLevelForKey(CGWindowLevelKey.StatusWindowLevelKey))
+			warnPanel.level = .statusBar
  			warnAlert.runModal();
-			NSApplication.sharedApplication().terminate(nil)
+		    NSApp.terminate(nil)
 		}
 		else
 		{
 			var matchedString : String? = getSecretTextFromKeychain()
 
 			// Override keychain stored value if option key is held down
-			let keyFlags = NSEvent.modifierFlags()
+		    let keyFlags : NSEvent.ModifierFlags = []
 
-			// We don't have a way of preventing compilation based on SDK version,
-			// so we have to rely upon Swift version correlating to the pertinent SDK
-			// version. .AlternateKeyMask is deined prior to 10.12 SDK, .Option after
-			#if swift(>=2.3)
-				let overrideKeychain = keyFlags.contains(.Option)
-			#else
-				let overrideKeychain = keyFlags.contains(.AlternateKeyMask)
-			#endif
+		    let overrideKeychain = keyFlags.contains(.option)
 
 			if ((matchedString == nil) || overrideKeychain)
 			{
@@ -128,7 +113,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, RSStringMatchingKeyboardTapD
 			if (matchedString == nil)
 			{
 				NSLog("No stored password and no user-supplied password. Quitting…")
-				NSApplication.sharedApplication().terminate(nil)
+				NSApp.terminate(nil)
 			}
 			else
 			{
@@ -137,8 +122,8 @@ class AppDelegate: NSObject, NSApplicationDelegate, RSStringMatchingKeyboardTapD
 		}
 
 		// Register our default ignored app identifiers
-		let defaultIgnoredApps : NSArray = ["com.apple.ScreenSharing"]
-		NSUserDefaults.standardUserDefaults().registerDefaults(["IgnoredAppIdentifiers": defaultIgnoredApps])
+		let defaultIgnoredApps : [String] = ["com.apple.ScreenSharing"]
+		UserDefaults.standard.register(defaults: ["IgnoredAppIdentifiers": defaultIgnoredApps])
 	}
 
 	func applicationWillTerminate(aNotification: NSNotification)
@@ -148,46 +133,37 @@ class AppDelegate: NSObject, NSApplicationDelegate, RSStringMatchingKeyboardTapD
 
 	func shouldIgnoreMatches() -> Bool
 	{
+		// Work around problems where Terminal password entry (e.g. sudo prompt) and Screen Sharing
+		// (e.g. legitimate secure entry into Screen Sharing app) cause false alarms because secure
+		// keyboard entry is not enabled while typing passwords.
+
 		var shouldIgnore : Bool = false
 
-		func ignoredAppIDs() -> NSArray
+		func ignoredAppIDs() -> [String]
 		{
-			if let myArray : NSArray = NSUserDefaults.standardUserDefaults().objectForKey("IgnoredAppIdentifiers") as? NSArray
-			{
-				return myArray
-			}
-			else
-			{
-				return NSArray()
-			}
+		    return UserDefaults.standard.stringArray(forKey: "IgnoredAppIdentifiers") ?? []
 		}
 
-		func frontAppID() -> String
+		guard let frontAppID = NSWorkspace.shared.frontmostApplication?.bundleIdentifier else { return false }
+
+		if (ignoredAppIDs().contains(frontAppID))
 		{
-			let frontApp : NSRunningApplication = NSWorkspace.sharedWorkspace().frontmostApplication!
-			return frontApp.bundleIdentifier!
-		}
-		// Work around problems where Terminal password entry (e.g. sudo prompt) and Screen Sharing
-		// (e.g. legitimate secure entry into Screen Sharing app) cause false alarms because secure 
-		// keyboard entry is not enabled while typing passwords.
-		if (ignoredAppIDs().containsObject(frontAppID()))
-		{
-			NSLog("Allowing blocked password typing in whitelisted app: %@.", frontAppID())
+			NSLog("Allowing blocked password typing in whitelisted app: %@.", frontAppID)
 			shouldIgnore = true
 		}
 
 		return shouldIgnore
 	}
 
-	func keyboardTap(theTap: RSStringMatchingKeyboardTap!, didMatchString theString: String!)
+    func keyboardTap(_ theTap: RSStringMatchingKeyboardTap!, didMatch theString: String!)
 	{
-		if (self.shouldIgnoreMatches() == false)
+		if (!shouldIgnoreMatches())
 		{
 			// Just put up a dialog to block the remaining keys of the password
-			NSApplication.sharedApplication().activateIgnoringOtherApps(true)
+		    NSApp.activate(ignoringOtherApps: true)
 			let dummyAlert = NSAlert()
 			dummyAlert.messageText = "Hey dummy, don't type your password where everybody can see it!"
-			dummyAlert.icon = NSImage(named:NSImageNameCaution)
+			dummyAlert.icon = NSImage(named: NSImage.cautionName)
 			dummyAlert.runModal()
 		}
 	}
